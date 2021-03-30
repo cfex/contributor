95302ee8231576d6c3ffc430f34327065ba1d5e8
cfex <jevtic.nenad.jevta@gmail.com>
2021-03-30 10:18:52 -0700

    Added payload models

---
+++ src/main/java/com/contributor/payload/request/UserRegisterRequest.java
@@ -0,0 +1,52 @@
+package com.contributor.payload.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserRegisterRequest {
+
+    @NotNull
+    @Size(min = 3, max = 50, message = "First name must contain between 3 and 50 characters")
+    private String name;
+
+    @NotNull
+    @Size(min = 3, max = 50, message = "First name must contain between 3 and 50 characters")
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 50, message = "First name must contain between 3 and 50 characters")
+    private String nickname;
+
+    @NotNull
+    @Email
+    @Size(min = 6, max = 100, message = "Email can't be empty and it should be between 6 and 100 characters.")
+    private String email;
+
+    private String school;
+
+    @Past
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @NotNull
+    @Size(min = 6, message = "Password must contain 6 or more characters. Please consider using " +
+            "capital letters and special characters")
+    private String password;
+
+}
