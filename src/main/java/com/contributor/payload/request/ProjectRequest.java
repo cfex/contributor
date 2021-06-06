09925649dc24fcf72c79ba4102e6a1033dbcb017
Jevtić Nenad <pekinezer.jevtic@gmail.com>
2021-06-06 21:44:03 +0200

    Payload refactor / Response optimization

--- src/main/java/com/contributor/payload/request/ProjectRequest.java
+++ src/main/java/com/contributor/payload/request/ProjectRequest.java
@@ -4,17 +4,31 @@ import com.contributor.model.enumeration.DevStatus;
 import com.contributor.model.user.User;
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class ProjectRequest {
+    @NotBlank
+    @Size(max = 200, min = 3, message = "The title must contain at least 3 characters and not more than 200")
     private String title;
+
+    @Size(max = 200, min = 3, message = "The intro must contain at least 3 characters and not more than 500")
     private String intro;
+
+    @NotBlank
+    @Size(max = 5000, min = 5, message = "The description must contain at least 5 characters and not more than 5000")
     private String description;
+
     private DevStatus devStatus;
-    private Boolean published;
+    private Boolean isPublished;
+
+    @NotNull
+    @Size(min = 5)
     private String github_url;
-    private User host; 
+
+    private User host;
 }                                                                          
